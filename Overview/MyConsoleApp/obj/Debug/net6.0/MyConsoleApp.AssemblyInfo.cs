e7050a3096261323ee372124ea97bc354093c246
Samuel-net <samuelonwuaso80@gmail.com>
2025-07-12 10:47:20 +0100

    CreateFiles

--- Overview/MyConsoleApp/obj/Debug/net6.0/MyConsoleApp.AssemblyInfo.cs
+++ Overview/MyConsoleApp/obj/Debug/net6.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20091b0e49f60bf6b848434f09663a8162fde7de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886ab04919c6105b092fb1ed5a1bf2a3571103c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
